942db8badea3224b80c3d61c1054062aa1302c1b
yarakoba <zangetsyyy@mail.ru>
2023-06-01 00:41:39 +0400

    min change

--- utils/config_init.py
+++ utils/config_init.py
@@ -10,6 +10,12 @@ def load_config():
 config = load_config()
 
 NOTIFY = config.getboolean('Settings', 'notify', fallback=False)
+DEBUG = config.getboolean('Settings', 'debug', fallback=False)
+TIMEOUT = config.get('Settings', 'timeout', fallback=10)
+CHECK_EXTERNAL = config.get('Settings', 'check_external')
+
+SITE_URL_CONFIG = config.get('input', 'site_url', fallback='')
+
 SENDER_MAIL = config.get('Mail', 'sender_mail', fallback='')
 SENDER_MAIL_PASSWORD = config.get('Mail', 'sender_mail_password', fallback='')
 RECEIVER_MAIL = config.get('Mail', 'receiver_email', fallback='')
@@ -17,8 +23,7 @@ EMAIL_PORT = config.get('Mail', 'email_port', fallback='')
 EMAIL_HOST = config.get('Mail', 'email_host', fallback='')
 
 
-DEBUG = config.getboolean('Settings', 'debug', fallback=False)
-TIMEOUT = config.get('Settings', 'timeout', fallback=10)
-SITE_URL_CONFIG = config.get('input', 'site_url', fallback='')
-CHECK_EXTERNAL = config.get('Settings', 'check_external')
+
+
+
 
